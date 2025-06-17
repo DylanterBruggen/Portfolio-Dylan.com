d63832ebdc032306503dc83ace21c43da6cb973e
dylan <0337452@student.rocvantwente.nl>
2025-06-17 20:17:10 +0200

    create portfolio repo V3

---
+++ Portfolio-Dylan-Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Portfolio_Dylan_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
