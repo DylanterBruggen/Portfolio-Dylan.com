0536139c6d35875bc2cb236237d809120fb88319
dylan <0337452@student.rocvantwente.nl>
2025-06-18 21:48:37 +0200

    commit 3

--- Portfolio-Dylan-Web/Controllers/HomeController.cs
+++ Portfolio-Dylan-Web/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace Portfolio_Dylan_Web.Controllers
             return View();
         }
 
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
