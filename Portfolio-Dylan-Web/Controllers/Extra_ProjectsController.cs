872bcbde04fd42e90da9b0bf91062c0154d4dec8
dylan <0337452@student.rocvantwente.nl>
2025-06-19 22:59:44 +0200

    basic site done 1

---
+++ Portfolio-Dylan-Web/Controllers/Extra_ProjectsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Dylan_Web.Controllers
+{
+    public class Extra_ProjectsController : Controller
+    {
+        public IActionResult Extra()
+        {
+            return View();
+        }
+    }
+}
--- Portfolio-Dylan-Web/Controllers/HomeController.cs
+++ Portfolio-Dylan-Web/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Portfolio_Dylan_Web.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
