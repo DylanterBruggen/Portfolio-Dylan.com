4b39851a2943cf4dd2e5ed2dff60079c4d742aaa
dylan <0337452@student.rocvantwente.nl>
2025-06-20 10:15:01 +0200

    basic site done

--- Portfolio-Dylan-Web/Controllers/AdminController.cs
+++ Portfolio-Dylan-Web/Controllers/AdminController.cs
@@ -1,14 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Portfolio_Dylan_Web.Controllers
 {
     public class AdminController : Controller
     {
+        [Authorize(Roles = "Admin")]
         public IActionResult Main_Admin()
         {
             return View();
         }
 
+        [Authorize(Roles = "Admin")]
         public IActionResult Logs()
         {
             return View();
